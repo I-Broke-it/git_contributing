853d8a8bac18041e8694782e453338b06f795a4b
Frank Baca <frankers@franks-mbp.lan>
2020-09-27 11:46:46 -0500

    Add script to configure dev env and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # git_contributing
+The goal of this project is to make project search and filtering on Github more accessible, thereby enabling users to find projects they can make valuable contributions to.
+
+## Setup
+
+First, set up an auth token with Github, copy it and pass it as an argument to --token in the api_creds_setup.py script. 
+This stores an encrypted version of the token along with the key in the environment you store the script in via environment variables
+
+Navigate to the project root folder via cli and run the following:
+```bash
+python api_creds_setup.py --token <your_github_token>
+```
+
+Next, source changes in your respective environment config file
+
+If bashrc then: 
+```bash
+source ~/.bashrc
+```
+If zshrc then: 
+```zsh
+source ~/.zshrc
+```
+
+Finally, verify the creation of environment variables GH_KEY and GH_TOKEN with the following line:
+```bash
+echo $GH_KEY '\n' $GH_TOKEN
+```
+You should see an output of two encrypted credentials after running this
---
+++ api_creds_setup.py
@@ -0,0 +1,40 @@
+from argparse import ArgumentParser, PARSER
+from base64 import urlsafe_b64encode
+import os
+import base64
+import argparse
+from cryptography.fernet import Fernet
+
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument_group('required arguments')
+PARSER.add_argument("--token", required=True, dest="GH_TOKEN", action="store")
+PROGRAM_ARGS = PARSER.parse_args()
+
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
+cipher_text = cipher_suite.encrypt(bytes(PROGRAM_ARGS.GH_TOKEN, 'ascii'))
+
+export_stmts = ['\nexport GH_KEY="' + key.decode('utf-8') + '"\n', 
+                'export GH_TOKEN="' + cipher_text.decode('utf-8') + '"\n']
+
+env_file_path = os.environ['HOME'] + '/.zshrc'
+#if MacOS, write encrypted token and key to .zshrc
+try:
+    if os.path.exists(env_file_path):
+        with open(env_file_path, mode='a+') as zshrc:
+            zshrc.writelines(export_stmts)
+            zshrc.close()
+except FileNotFoundError:
+    print("zshrc not found")
+
+
+env_file_path = os.environ['HOME'] + '/.bashrc'
+
+#if Ubuntu or similar, write encrypted token and key to .bashrc
+try:
+    if os.path.exists(env_file_path):
+        with open(os.environ['HOME'] + '/.bashrc', mode='a+') as bashrc:
+            bashrc.writelines(export_stmts)
+            bashrc.close()
+except FileNotFoundError:
+    print("bashrc not found")
