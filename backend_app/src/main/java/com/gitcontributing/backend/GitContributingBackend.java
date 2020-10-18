c5b8bacabc44054ec25ebc39914158ce9ffa1e2e
Frank Baca <frankers@franks-mbp.lan>
2020-10-18 13:00:36 -0500

    Add basic skeleton for spring boot backend app, add .gitignore

---
+++ backend_app/src/main/java/com/gitcontributing/backend/GitContributingBackend.java
@@ -0,0 +1,13 @@
+package com.gitcontributing.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitContributingBackend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitContributingBackend.class, args);
+	}
+
+}
